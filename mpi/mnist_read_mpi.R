d633f5d1b4e88da2939774b0b9ca6cd7d6b1cfa3
George Ostrouchov <ostrouchovg@ornl.gov>
2022-04-27 01:15:04 -0400

    format

--- mpi/mnist_read_mpi.R
+++ mpi/mnist_read_mpi.R
@@ -1,32 +1,34 @@
-suppressMessages( library( rhdf5 ) )
-suppressMessages( library( pbdIO ) )
+suppressMessages(library(rhdf5))
+suppressMessages(library(pbdIO))
 
 filename = "/scratch/project/dd-21-42/data/mnist/train.hdf5"
 dataset1  = "image"
 dataset2  = "label"
 
 ## get and broadcast dimensions to all processors
-if ( comm.rank() == 0 ) {
-   h5f = H5Fopen( filename, flags="H5F_ACC_RDONLY")
-   h5d = H5Dopen( h5f, dataset1 )
-   h5s = H5Dget_space( h5d )
-   dims = H5Sget_simple_extent_dims( h5s )$size
-   H5Dclose( h5d )
-   H5Fclose( h5f )
+if (comm.rank() == 0) {
+   h5f = H5Fopen(filename, flags="H5F_ACC_RDONLY")
+   h5d = H5Dopen(h5f, dataset1)
+   h5s = H5Dget_space(h5d)
+   dims = H5Sget_simple_extent_dims(h5s)$size
+   H5Dclose(h5d)
+   H5Fclose(h5f)
 } else dims = NA
-dims = bcast( dims ) 
+dims = bcast(dims) 
 
 ## get my local indices for contiguous data read
 nlast = dims[length(dims)] # last dim moves slowest
-my_ind = comm.chunk( nlast, form = "vector" )
+my_ind = comm.chunk(nlast, form = "vector")
 
 ## parallel read of local data
-my_train = as.double( h5read( filename, dataset1, index = list( NULL, NULL, my_ind ) ))
-my_train_lab = as.character( h5read( filename, dataset2, index = list( my_ind ) ) )
-H5close( )
+my_train = as.double(h5read(filename, dataset1, 
+                            index = list(NULL, NULL, my_ind)))
+my_train_lab = as.character(h5read(filename, dataset2, 
+                                   index = list(my_ind)))
+H5close()
 
-dim( my_train ) = c( prod( dims[-length(dims)] ), length(my_ind) )
-my_train = t( my_train )  # now it's rowblock
+dim(my_train) = c(prod(dims[-length(dims)]), length(my_ind))
+my_train = t(my_train)  # now it's in rowblock positions
 
 ## plot for debugging
 # if(comm.rank() == 0) {
@@ -42,6 +44,7 @@ my_train = t( my_train )  # now it's rowblock
 #   ggplot(im, aes(row, col, fill = val)) + geom_tile() + facet_wrap(~ image)
 # }
 
-comm.print( dim(my_train), all.rank = TRUE )
+comm.print(dim(my_train), all.rank = TRUE)
 
-finalize( )
+## remove finalize if sourced in another script
+# finalize()
