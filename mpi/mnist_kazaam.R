34462c853e81045117ea28b6e4d92eaf2a4858ef
George Ostrouchov <ostrouchovg@ornl.gov>
2022-05-03 23:10:28 -0400

    update

--- mpi/mnist_kazaam.R
+++ mpi/mnist_kazaam.R
@@ -1,15 +1,12 @@
-suppressMessages(library(kazaam))
 source("mnist_read_mpi.R") # reads blocks of rows
+suppressMessages(library(kazaam))
 
-## create shaq class distributed matrix from local pieces
+## construct shaq matrix
 sq_train = shaq(my_train)
 
 ## svd (shaq class: tall-skinny matrix)
-train_svd = svd(sq_train, nu = 0, nv = 5)
-comm.cat("kazaam top(5) singular values:", train_svd$d[1:5], "\n")
-
-cp_train = allreduce(crossprod(my_train))
-train_svd2 = eigen(cp_train)
-comm.cat("direct top(5) singular values:", sqrt(train_svd2$values[1:5]), "\n")
+options(warn = -1) ## suppress warnings about negative eigenvalues for mnist
+train_svd = svd(sq_train, nu = 0, nv = 10)
+comm.cat("kazaam top(5) singular values:", train_svd$d[1:10], "\n")
 
 finalize()
--- mpi/mnist_read_mpi.R
+++ mpi/mnist_read_mpi.R
@@ -1,14 +1,13 @@
 suppressMessages(library(rhdf5))
 suppressMessages(library(pbdIO))
-
-filename = "/scratch/project/dd-21-42/data/mnist/train.hdf5"
-dataset1  = "image"
-dataset2  = "label"
+file = "/scratch/project/dd-21-42/data/mnist/train.hdf5"
+dat1  = "image"
+dat2  = "label"
 
 ## get and broadcast dimensions to all processors
 if (comm.rank() == 0) {
-   h5f = H5Fopen(filename, flags="H5F_ACC_RDONLY")
-   h5d = H5Dopen(h5f, dataset1)
+   h5f = H5Fopen(file, flags="H5F_ACC_RDONLY")
+   h5d = H5Dopen(h5f, dat1)
    h5s = H5Dget_space(h5d)
    dims = H5Sget_simple_extent_dims(h5s)$size
    H5Dclose(h5d)
@@ -16,19 +15,16 @@ if (comm.rank() == 0) {
 } else dims = NA
 dims = bcast(dims) 
 
-## get my local indices for contiguous data read
 nlast = dims[length(dims)] # last dim moves slowest
 my_ind = comm.chunk(nlast, form = "vector")
 
-## parallel read of local data
-my_train = as.double(h5read(filename, dataset1, 
-                            index = list(NULL, NULL, my_ind)))
-my_train_lab = as.character(h5read(filename, dataset2, 
-                                   index = list(my_ind)))
+## parallel read of data columns
+my_train = as.double(h5read(file, dat1, index = list(NULL, NULL, my_ind)))
+my_train_lab = as.character(h5read(file, dat2, index = list(my_ind)))
 H5close()
 
 dim(my_train) = c(prod(dims[-length(dims)]), length(my_ind))
-my_train = t(my_train)  # now it's in rowblock positions
+my_train = t(my_train)  # row-major write and column-major read
 
 ## plot for debugging
 # if(comm.rank() == 0) {
@@ -43,8 +39,6 @@ my_train = t(my_train)  # now it's in rowblock positions
 #                   val = as.numeric(unlist(train[ivals, ])))
 #   ggplot(im, aes(row, col, fill = val)) + geom_tile() + facet_wrap(~ image)
 # }
-
-comm.print(dim(my_train), all.rank = TRUE)
 
 ## remove finalize if sourced in another script
 # finalize()
